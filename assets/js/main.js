0230dde9b73268191d968a93226c395335f04c51
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2023-02-21 20:13:05 -0600

    Object activity

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
 console.log("Hola 🤖");
+/* ------------------- */
+const curso = {
+  nombre: "UCAMP",
+  tipos: "FullStack",
+  modulos: [
+    { nombre: "css", cursado: true },
+    { nombre: "JavaScript", cursado: true },
+    { nombre: "React", cursado: false },
+  ],
+  saludar: (nombreDePersona) => {
+    console.log(`Hola, mucho gusto ${nombreDePersona}`);
+  },
+};
+
+/* Impresión en consola */
+console.log(typeof curso);
+/* Aquí va a imprimir el nombre de la empresa: UCAMP */
+console.log(curso.nombre);
+
+/* Aquí quiero imprimir el nombre JavaScript del objeto que se encuentra en mi array módulos */
+console.log(curso.modulos[1].nombre);
