303534c70614d12d8e04cc94957886f24250d29e
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2023-02-21 20:41:04 -0600

    DOM with JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,34 @@ console.log(curso.nombre);
 
 /* Aquí quiero imprimir el nombre JavaScript del objeto que se encuentra en mi array módulos */
 console.log(curso.modulos[1].nombre);
+
+/* Vamos a hacer uso del DOM para meter datos en el HTML */
+document.getElementById("dom").innerHTML = `
+<li>${curso.nombre}</li>
+
+<table style="height: 205px;" width="440">
+<tbody>
+<tr>
+<td style="width: 139.328px;">No.</td>
+<td style="width: 139.328px;">Nombre</td>
+<td style="width: 139.344px;">¿Cursado?</td>
+</tr>
+<tr>
+<td style="width: 139.328px;">1</td>
+<td style="width: 139.328px;">${curso.modulos[0].nombre}</td>
+<td style="width: 139.344px;">${curso.modulos[0].cursado}</td>
+</tr>
+<tr>
+<td style="width: 139.328px;">2</td>
+<td style="width: 139.328px;">${curso.modulos[1].nombre}</td>
+<td style="width: 139.344px;">${curso.modulos[1].cursado}</td>
+</tr>
+<tr>
+<td style="width: 139.328px;">3</td>
+<td style="width: 139.328px;">${curso.modulos[2].nombre}</td>
+<td style="width: 139.344px;">${curso.modulos[2].cursado}</td>
+</tr>
+</tbody>
+</table>
+
+`;
